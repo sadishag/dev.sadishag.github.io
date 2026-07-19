d038af5c9f0dd6e635ec8dc847b852a7feae881d
sadishag <sadishag@gmail.com>
2026-07-18 22:48:28 -0400

    Adding a video section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
+import Video from "./components/Video";
 import Photography from "./components/Photography";
 import BlogList from "./components/BlogList";
 import Footer from "./components/Footer";
@@ -31,6 +32,7 @@ export default function App() {
         <About />
         <Experience />
         <Projects />
+        <Video />
         <Photography />
         <BlogList />
       </main>
